9c417ed284e7349da11f11e3c3db974f7a0b0bdb
Watthanachai Dueanklang <bosskillerz@gmail.com>
2018-09-25 16:10:58 +0700

    modified:   opt.c 	modified:   opt.h

--- opt.c
+++ opt.c
@@ -1,66 +1,56 @@
-#include <string.h>
 #include "opt.h"
+#include <string.h>
 
-#define DINDEX -1		/*Dont Edit This value, This value must be negative */
+static int i_upindex = DUPINDEX;
 
-static int i_upindex = DINDEX;
-
-int
-opt_action (int argc, const char *argv[], const char *param[], char *buff,
-	    unsigned int bsize, int start)
+int opt_action(int argc, const char* const argv[], const char* const param[], char* const buff,
+    unsigned int bsize, int start)
 {
-  int i, j, k, l;
+    int i, j, l;
+    unsigned int k;
 
-  buff[0] = 0;
+    buff[0] = 0;
 
+    for (j = (i_upindex >= 0) ? i_upindex : start; j < argc; j++) {
 
-  for (j = (i_upindex >= 0) ? i_upindex : start; j < argc; j++)
-    {
+        for (i = 0; param[i]; i++) {
 
-      for (i = 0; param[i]; i++)
-	{
+            if (!(strncmp(argv[j], param[i], strlen(param[i])))) {
+                for (k = 0, l = strlen(param[i]);
+                     argv[j][l]; k++, l++) {
+                    if (k + 1 >= bsize)
+                        return e_optoom;
+                    buff[k] = argv[j][l];
+                }
+                buff[k] = 0;
+                i_upindex = j + 1;
+                return i;
+            }
+        }
 
-	  if (!(strncmp (argv[j], param[i], strlen (param[i]))))
-	    {
-	      for (k = 0, l = strlen (param[i]);
-		   argv[j][l] && (k < bsize - 1); k++, l++)
-		{
-		  buff[k] = argv[j][l];
-		}
-	      buff[k] = 0;
-	      i_upindex = j + 1;
-	      return i;
-	    }
-	}
-
-      for (k = 0, l = 0; argv[j][l] && (k < bsize - 1); k++, l++)
-	{
-	  buff[k] = argv[j][l];
-	}
-      buff[k] = 0;
-      i_upindex = j + 1;
-      return e_optother;
-
-
-
+        for (k = 0, l = 0; argv[j][l]; k++, l++) {
+            if (k + 1 >= bsize)
+                return e_optoom;
+            buff[k] = argv[j][l];
+        }
+        buff[k] = 0;
+        i_upindex = j + 1;
+        return e_optother;
     }
-  return e_optend;
+    return e_optend;
 }
 
-int
-opt_getIndex (void)
+int opt_getIndex(void)
 {
-  return i_upindex;
+    return i_upindex;
 }
 
-void
-opt_setIndex (int index)
+void opt_setIndex(int index)
 {
-  i_upindex = index;
+    i_upindex = index;
 }
 
-void
-opt_resetIndex (void)
+void opt_resetIndex(void)
 {
-  i_upindex = DINDEX;
+    i_upindex = DUPINDEX;
 }
